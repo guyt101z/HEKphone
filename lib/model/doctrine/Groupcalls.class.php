cb371ea02904768089d6b9f825a5a0f32a9e3508
Hannes Maier-Flaig <hamfbohne@gmail.com>
2011-01-19 12:59:20 +0100

    commented the code, renamed internal variables, groupcall->getExtensionsAsArray() now returns an empty array if there's no resident in the groupcall

--- lib/model/doctrine/Groupcalls.class.php
+++ lib/model/doctrine/Groupcalls.class.php
@@ -12,10 +12,21 @@
  */
 class Groupcalls extends BaseGroupcalls
 {
+  /**
+   * Returns an array of groupcalls extensions for the asterisk dialplan. Intended for use with
+   * AsteriskExtensions->fromArray()
+   *
+   * @return array $arayExtensions
+   */
   public function getExtensionsAsArray() {
     $context = 'groupcalls';
-    $phonesList = '';
+
+    /* We're creating an extension that will dial to the phones of all Residents in the Groupcall
+     * simultaniousely. We just need to connect the dialstrings with & for asterisk for this.
+     */
+    $dialstring = '';
     foreach($this->Residents as $Resident) {
+        // Analog phones and sip phones have a slightly different dialstring.
         if($Resident->Rooms->Phones->get('technology') == 'DAHDI/g1')
         {
           $prefix = '86951';
@@ -23,17 +34,26 @@ class Groupcalls extends BaseGroupcalls
           $prefix = '1';
         }
 
-        $phonesList .= $Resident->Rooms->Phones->get('technology') . '/' . $prefix . $Resident->Rooms . ASTERISK_PARAMETER_SEPARATOR;
+        // Append to the dialstring
+        $dialstring .= $Resident->Rooms->Phones->get('technology') . '/' . $prefix . $Resident->Rooms . '&';
         ;
     }
-    $phonesList = substr($phonesList, 0, -1 * strlen(ASTERISK_PARAMETER_SEPARATOR)); //strip the last parameter separator
+
+    // Return an empty array, if there are no residents in the groupcall.
+    if($dialstring == ''){
+        return array();
+    }
+
+    // Strip the last &
+    $dialstring = substr($dialstring, 0, -1);
+
 
     $arrayExtensions[0] = array(
          'exten'        => $this->extension,
          'priority'     => 1,
          'context'      => $context,
          'app'          => 'Dial',
-         'appdata'      => $phonesList
+         'appdata'      => $dialstring
     );
     $arrayExtensions[1] = array(
          'exten'        => $this->extension,
