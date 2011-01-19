72c32928f520e5a1aef239c8f2d05b50a0d0a173
Hannes Maier-Flaig <hamfbohne@gmail.com>
2011-01-19 12:12:38 +0100

    added linebreaks

--- apps/frontend/templates/_movingInMail.php
+++ apps/frontend/templates/_movingInMail.php
@@ -2,12 +2,12 @@
 
                               Gratulation!
                            ******************
- 
+
    * Dein Telefonanschluss wurde soeben auch fuer abgehende Amtsgespraeche
      freigeschaltet.
    * Das Amt kannst Du erreichen, indem Du zuerst die 0 waehlst. Dann
      hoerst Du einen Dauerton. Jetzt wie in einer Telefonzelle waehlen.
-     (Bsp.: zu mir nach hause waehle ich 0 072718xxx)   
+     (Bsp.: zu mir nach hause waehle ich 0 072718xxx)
    * Notruf ist bei ALLEN Telefonen (ausser am Klingelboard) IMMER ueber
      <*112> zu erreichen.
    * An diese (deine) Email-Adresse werden auch die
@@ -24,16 +24,19 @@ Bitte ueberpruefe, ob sich auch kein Tippfehler in deiner Kontoverbindung einges
 Dies wuerde zu einer Ruecklastschrift fuehren, die mindestens 10 Euro Gebuehren verursacht.
 
 Kontonummer   : <?php echo $bank_number ?>
+
 Bankleitzahl  : <?php echo $account_number ?>
+
 Rechnungsemail: <?php echo $email ?>
 
+
 ------------------------------------------------------------------------------------
-   
+
                             Congratulations
                         ************************
-     
+
    * Your HEK-phone has been enabled to dial outgoing calls.
-   * To get outside line access you first have to dial 0 then you'll hear a 
+   * To get outside line access you first have to dial 0 then you'll hear a
      continuous tone. Now go on as if you are in a telephonebox. (example.:
      I want to call my family. So I call this number  0 072718xxx).
    * Emergency call is <*112> with ALL phones (except the one at the
@@ -50,9 +53,9 @@ If you need manuals for quite everything please visit our homepage
 http://hekphone.uni-karlsruhe.de
 
 Please check that there is no typo in your bank account details. This would
-cause your bank not to fullfill our withdrawls and would lead to costs at a minimum 
+cause your bank not to fullfill our withdrawls and would lead to costs at a minimum
 of 10 Euro.
 
-Your bank account number (\"Kontonummer\"): <?php echo $bank_number ?>
-Bank number (\"Bankleitzahl, BLZ\")       : <?php echo $account_number ?>
-E-mail account for your invoices        : <?php echo $email ?>
+Your bank account number (\"Kontonummer\"): <?php echo $bank_number . PHP_EOL?>
+Bank number (\"Bankleitzahl, BLZ\")       : <?php echo $account_number . PHP_EOL ?>
+E-mail account for your invoices        : <?php echo $email . PHP_EOL ?>
