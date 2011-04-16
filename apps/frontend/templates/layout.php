e29177bbae2edfa565bb48074994246065a8ab5b
Hannes Maier-Flaig <hamfbohne@gmail.com>
2011-04-16 18:16:26 +0200

    i18n fixed: flash messages have not been translated due to missing __(...)

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -39,13 +39,13 @@
 
     <?php if ($sf_user->hasFlash('notice')): ?>
     <div class="notice">
-    <?php echo $sf_user->getFlash('notice') ?>
+    <?php echo __($sf_user->getFlash('notice')) ?>
     </div>
     <?php endif;?>
 
     <?php if ($sf_user->hasFlash('error')): ?>
     <div class="error">
-    <?php echo $sf_user->getFlash('error') ?>
+    <?php echo __($sf_user->getFlash('error')) ?>
     </div>
     <?php endif;?>
 
