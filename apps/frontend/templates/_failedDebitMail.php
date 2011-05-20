538ff648524553be77cc1c0bb865b56593012a39
Constantin <constantin.wehmschulte@gmail.com>
2011-05-20 22:49:38 +0200

    initial commit.

---
+++ apps/frontend/templates/_failedDebitMail.php
@@ -0,0 +1,26 @@
+[english version follows at the bottom]
+
+Hallo <?php echo $firstName ?>,
+
+leider wurde beim Einziehen des Rechnungsbetrags deiner letzten Rechnung eine
+Ruecklastschrift erzeugt. Das passiert wenn dein Konto z.B. nicht ausreichend
+gedeckt ist oder es nicht mehr existiert. 
+
+Eine Ruecklastschrift erzeugt Kosten in Hoehe von 10 Euro. Bitte bring das Geld 
+(Rechnungsbetrag + 10 Euro) moeglichst schnell bei einem HEKphone-Tutor vorbei. 
+
+Dein Telefonanschluss ist so lange gesperrt, bis du die Kosten beglichen hast!
+
+--------------------------------------------------------------------------------
+
+Hello <?php echo $firstName ?>,
+
+unfortunately the debit of your last billing amount has failed. This happens when
+your bank account is not covered enough or the account is closed. 
+
+This failed debit costs 10 Euro. Please bring the money (billing amount + 10 Euro) 
+to a member of the hekphone team.
+
+Your telefone is locked until you have paid for the bill and the costs.  
+ 
+ 
