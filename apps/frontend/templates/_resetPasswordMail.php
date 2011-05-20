88dfa844a8b9e2624a5f9a8452d164ad20b2e40e
Constantin <constantin.wehmschulte@gmail.com>
2011-05-20 22:36:14 +0200

    Initial Commit. Mail template to inform the user about a reseted password for the frontend

---
+++ apps/frontend/templates/_resetPasswordMail.php
@@ -0,0 +1,22 @@
+[english version follows at the bottom]
+
+Hallo <?php echo $firstName ?>,
+
+dein Passwort im HEKphone-Portal (http://hekphone) wurde soeben neu gesetzt.
+
+Es lautet:  <?php echo $password . PHP_EOL ?>
+
+Im Portal kannst du das Passwort jederzeit auf ein Passwort deiner Wahl aendern. 
+
+
+-------------------------------------------------------------------------------
+
+
+Hello <?php echo $firstName ?>,
+
+your password for the hekphone-frontend (http://hekphone) is now reseted. 
+
+Your new password is: <?php echo $password . PHP_EOL ?>
+
+You can change this password to a password of your choice in the frontend at 
+any time. 
